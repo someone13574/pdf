c81235e9a5f2a69ee84091b3c7f9ee45111ce45a
Owen Law <owenlaw222@gmail.com>
2025-09-06 01:45:04 -0400

    Implement more content op deserialization

--- libs/pdf/include/pdf/content_op.h
+++ libs/pdf/include/pdf/content_op.h
@@ -4,12 +4,25 @@
 #include "pdf/types.h"
 
 typedef enum {
+    PDF_CONTENT_OP_SET_CTM,    // cm
     PDF_CONTENT_OP_BEGIN_TEXT, // BT
     PDF_CONTENT_OP_END_TEXT,   // ET
     PDF_CONTENT_OP_SET_FONT,   // Tf
     PDF_CONTENT_OP_NEXT_LINE,  // Td
-    PDF_CONTENT_OP_SHOW_TEXT   // Tj
+    PDF_CONTENT_OP_SET_TM,     // Tm
+    PDF_CONTENT_OP_SHOW_TEXT,  // Tj
+    PDF_CONTENT_OP_SET_GRAY    // G, g
 } PdfContentOpKind;
+
+/// Used by PDF_CONTENT_OP_SET_CTM, PDF_CONTENT_OP_SET_TM
+typedef struct {
+    PdfNumber a;
+    PdfNumber b;
+    PdfNumber c;
+    PdfNumber d;
+    PdfNumber e;
+    PdfNumber f;
+} PdfContentOpSetMatrix;
 
 typedef struct {
     PdfName font;
@@ -26,12 +39,19 @@ typedef struct {
 } PdfContentOpShowText;
 
 typedef struct {
+    bool stroking;
+    PdfNumber gray;
+} PdfContentOpSetGray;
+
+typedef struct {
     PdfContentOpKind kind;
 
     union {
+        PdfContentOpSetMatrix set_matrix;
         PdfContentOpSetFont set_font;
         PdfContentOpNextLine next_line;
         PdfContentOpShowText show_text;
+        PdfContentOpSetGray set_gray;
     } data;
 } PdfContentOp;
 
